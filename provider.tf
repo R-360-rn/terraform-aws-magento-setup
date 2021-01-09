b371e0b636d29abf6cb9ffa4a7f528fc09f334da
R360rn <salahaddinov.nurlan@gmail.com>
2021-01-09 11:16:09 +0100

    initial commit

---
+++ provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 2.70"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "eu-central-1"
+  access_key = "PROD_ACCESS_KEY"
+  secret_key = "PROD_SECRET_KEY"
+}
+
